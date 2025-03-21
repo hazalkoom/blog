b14c0510e0eda11433c27a70b4e67fe5da232e4f
hazalkoom <mwhamd345@gmail.com>
2025-03-21 15:06:25 +0200

    Finish custom filters

--- bblog/settings.py
+++ bblog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
