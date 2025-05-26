5afd433e041aac02c35edc06c64825dfe50dd038
hazalkoom <mwhamd345@gmail.com>
2025-05-26 06:32:36 +0300

    Finish related fields

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
-from blog.api.serializers import PostSerializer
+from blog.api.serializers import PostSerializer, UserSerializer
 from blog.models import Post
 from .permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
-
+from django_auth.models import User
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
@@ -12,3 +12,8 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    
+class UserDetail(generics.RetrieveAPIView):
+    lookup_field = "email"
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
